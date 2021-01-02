b9ea1a1503167c02e0ba681dde995632bc73b7c9
alan_videabiz <alan@videabiz.com>
2021-01-03 00:56:35 +0700

    alan nguyen

--- publisher-web/app/modules/api/controllers/RestController.php
+++ publisher-web/app/modules/api/controllers/RestController.php
@@ -5,6 +5,7 @@ namespace Publisher\Modules\Api\Controllers;
 use Phalcon\Mvc\Controller;
 use Publisher\Common\Models\Bill\Bill;
 use Publisher\Common\Models\Bill\BillDetail;
+use Publisher\Common\Models\Bill\Conveyor;
 use Publisher\Common\Models\Bill\Product;
 use Publisher\Common\Models\Bill\TimeinTimeout;
 use Publisher\Common\Models\Users\Major;
